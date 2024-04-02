bc8906798eceb11d1bed0dcd4317df56fb319c70
Duarte Fernandes <duartefq@outlook.com>
2024-04-02 19:15:01 -0400

    chore: change page title, fix lint issues

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
 import ProfileCard from './components/ProfileCard.vue'
 </script>
 
